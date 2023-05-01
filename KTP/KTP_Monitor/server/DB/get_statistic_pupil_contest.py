203e27e600abcb99e1d1a490b09c97e1ec48cadb
Natlis <natalja.sobjanina15@gmail.com>
2023-05-01 15:10:19 +0300

    add check unique nick and delete_contest

--- KTP/KTP_Monitor/server/DB/get_statistic_pupil_contest.py
+++ KTP/KTP_Monitor/server/DB/get_statistic_pupil_contest.py
@@ -1,15 +1,36 @@
-from KTP_Monitor.models import Contest_Info
+# from KTP_Monitor.models import Contest_Info
+# from KTP_Monitor.models import Task_Info
+# from KTP_Monitor.models import Pupil_Task
 
 
-def get_statistic_pupils_contests(pupils, contests):
-	
-	result = []
-	for p in pupils:
-		print()
+# def get_statistic_contest(pupils, links):
 
-	print("status done")
+#     contests = Contest_Info.objects.get(link__in = links)
+#     for c in contests:
+#         tasks = list(Task_Info.objects.filter(contest = c))
+        
 
-	return d
+#     result = {}
+
+#     for t in tasks:
+#         result[t.letter] = {}
+#         print("add key " + t.letter)
+#         q = Pupil_Task.objects.filter(task = t)
+        
+#         for item in q:
+#             result[item.task.letter][item.pupil.CF] = (item.cnt_try, item.result)
+
+    
+#     # for p in result:
+#     #     print(p, sep=' ')
+#     #     for t in result[p]:
+#     #         print(result[p][t], sep= ' ')
+#     #     print()
+
+#     print("contest statistic done")
+
+#     return result
+
 
 
 
