c1b31e0eb6e58f7662b15e3912f932fb16c6edeb
Natlis <natalja.sobjanina15@gmail.com>
2023-05-02 11:34:07 +0300

    add check nick in all users

--- KTP/KTP_Monitor/server/DB/add_new_pupil.py
+++ KTP/KTP_Monitor/server/DB/add_new_pupil.py
@@ -1,11 +1,13 @@
 import mysql.connector
 from KTP_Monitor.models import Pupil_Info
+from KTP_Monitor.models import Teacher_Info
+
 # [user.get_id()], ['lastname'], ['firstname'],  ['secondname'], 
 #  ['nickname'], ['birthdate'],  
 # ['school'], ['grade'],['phone'])
 def add_new_pupil(user, lastname, firstname, secondname, CF, birthday, school, grade, phone):
 
-    if (Pupil_Info.objects.filter(CF = CF).exists()):
+    if (Pupil_Info.objects.filter(CF = CF).exists() or Teacher_Info.objects.filter(CF = CF).exists()):
         return False
 
     Pupil_Info.objects.create(user=user, lastname=lastname, firstname=firstname, secondname=secondname,
