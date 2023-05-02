5d0a37387f56c4ab23eb1ffdfcc5e3575a9ec2c4
Natlis <natalja.sobjanina15@gmail.com>
2023-05-02 11:07:30 +0300

    add check to unique contest/task

--- KTP/KTP_Monitor/server/DB/add_task.py
+++ KTP/KTP_Monitor/server/DB/add_task.py
@@ -3,9 +3,11 @@ from KTP_Monitor.models import Task_Info
 
 def add_task(link, letter, name):
     contest = Contest_Info.objects.get(link = link)
-    Task_Info.objects.get_or_create(contest = contest, letter = letter, name = name)
+    t =Task_Info.objects.get_or_create(contest = contest, letter = letter, name = name)
     
     print("task added")
+    return t[1]
+
 
 	
 	
