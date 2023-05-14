f070081713de8e1442075ceb62ebbf7fe1c32b3b
PussyDestroyer69 <dan.rojtburd@mail.ru>
2023-05-14 23:47:27 +0300

    fix bag stats contest

--- KTP/KTP_Monitor/server/stats.py
+++ KTP/KTP_Monitor/server/stats.py
@@ -7,43 +7,35 @@ def div_stats(division):
 
 def contest_stats(link):
     all_problem = []
-    all_problem2 = dict()
-    contests = get_all_contests()
-    for info in contests:
-        if link == info.link:
-            problem = dict()
-            problem['name'] = info.name
-            all_problem2[info.index] = info.name
-            all_problem.append(problem)
+    all_people = []
     result = get_statistic_contest(link)
+    people = dict()
     write_stat = dict()
     write_stat["status"] = True
-    all_people = {}
     name = set()
-    for task_letter in result:
-        for pupil_nick in result[task_letter]:
-            info = result[task_letter][pupil_nick].split()
-            if pupil_nick in name:
-                problem = all_people[pupil_nick]
-                problem.append({all_problem2[task_letter]: info[1]})
-                all_people[pupil_nick] = problem
+    ch = 0
+    for task in result:
+        all_problem.append({'name': task.name})
+        for pupil in result[task]:
+            info = result[task][pupil].split()
+            if pupil.CF in name:
+                problem = all_people[1]['problems']
+                problem.append({task.name: info[1]})
+                all_people[people[pupil.CF]]['problems'] = problem
             else:
+                info_people = dict()
                 problem = []
-                problem.append({all_problem2[task_letter]: info[1]})
-                all_people[pupil_nick] = problem
-                name.add(pupil_nick)
-    pupils = []
-    people = get_all_pupils()
-    for pupil_nick in people:
-        if pupil_nick.CF in name:
-            info = dict()
-            info["name"] = pupil_nick.firstname
-            info["secondname"] = pupil_nick.secondname
-            info["surname"] = pupil_nick.lastname
-            info["nickname"] = pupil_nick.CF
-            info["problems"] = all_people[pupil_nick.CF]
-            pupils.append(info)
-    write_stat["pupils"] = pupils
+                info_people['name'] = pupil.firstname
+                info_people['secondname'] = pupil.secondname
+                info_people['surname'] = pupil.secondname
+                info_people['nickname'] = pupil.CF
+                problem.append({task.name: info[1]})
+                info_people['problems'] = problem
+                all_people.append(info_people)
+                people[pupil.CF] = ch
+                ch = ch + 1
+                name.add(pupil.CF)
+    write_stat["pupils"] = all_people
     write_stat["problems"] = all_problem
     return write_stat
 
