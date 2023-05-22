008f4ce86b9aa1db6fdce41da6754969e7bd85f1
Natlis <natalja.sobjanina15@gmail.com>
2023-05-22 21:43:52 +0300

    all func return something

--- KTP/KTP_Monitor/server/DB/delete_contest.py
+++ KTP/KTP_Monitor/server/DB/delete_contest.py
@@ -4,4 +4,5 @@ def delete_contest(link):
 	contest = Contest_Info.objects.get(link = link)
 	contest.delete()
     
-	print("contest deleted")
+	# print("contest deleted")
+	return True
--- KTP/KTP_Monitor/server/DB/update_contest_time.py
+++ KTP/KTP_Monitor/server/DB/update_contest_time.py
@@ -2,6 +2,7 @@ from KTP_Monitor.models import Contest_Info
 
 def update_contest_time(link, time):
    Contest_Info.objects.filter(link = link).update(last_update = time)
+   return True
    
 
 
